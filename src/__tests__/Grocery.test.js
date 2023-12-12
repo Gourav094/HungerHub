3d511c57e122d7910fe0c6fd1b7d682508b948f0
gourav garg <garggourav012@gmail.com>
2023-12-12 20:21:29 +0530

    Done Unit Testing

--- src/__tests__/Grocery.test.js
+++ src/__tests__/Grocery.test.js
@@ -3,31 +3,36 @@ import "@testing-library/jest-dom"
 import Grocery from "../components/Grocery"
 import { renderMatches } from "react-router-dom"
 
-test("should load contact us component",() => {
-    render(<Grocery/>)
+describe("Grocery page test",() => {
+    it("should load grocery component",() => {
+        render(<Grocery/>)
+    
+        const heading = screen.getByRole("heading")
+        expect(heading).toBeInTheDocument();
+    })
 
-    const heading = screen.getByRole("heading")
-    expect(heading).toBeInTheDocument();
-})
-test("should load button inside contact component",() => {
-    render(<Grocery/>)
+    it("should load button inside grocery component",() => {
+        render(<Grocery/>)
+    
+        const button = screen.getByRole("button")
+    
+        expect(button).toBeInTheDocument();
+    })
 
-    const button = screen.getByRole("button")
+    it("should load input name inside grocery compoent",() => {
+        render(<Grocery/>)
+        const inputName = screen.getByPlaceholderText("name")
+    
+        expect(inputName).toBeInTheDocument()
+    })
 
-    expect(button).toBeInTheDocument();
-})
-test("should load input name inside contact compoent",() => {
-    render(<Grocery/>)
-    const inputName = screen.getByPlaceholderText("name")
-
-    expect(inputName).toBeInTheDocument()
-})
-
-test("should load mutiple input boxes",() => {
-    render(<Grocery/>)
-    const inputBoxes = screen.getAllByRole("textbox")
-
-    // console.log(inputBoxes.length)
-    expect(inputBoxes.length).toBe(2)
+    it("should load mutiple input boxes",() => {
+        render(<Grocery/>)
+        const inputBoxes = screen.getAllByRole("textbox")
+    
+        // console.log(inputBoxes.length)
+        expect(inputBoxes.length).toBe(2)
+    })
+      
 })
 
