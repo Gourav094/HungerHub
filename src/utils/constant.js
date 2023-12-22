a6648187cebb11ad246b26c934f2dfca42ec9387
gourav garg <garggourav012@gmail.com>
2023-12-22 10:44:35 +0530

    Update constant file

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,3 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.6871407&lng=76.6646509&restaurantId=";
+export const MENU_API = "https://kind-puce-bull-tie.cyclic.app/api/proxy/swiggy/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.6871407&lng=76.6646509&restaurantId=";
